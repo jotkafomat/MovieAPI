3d0bc91ccc361f08fea0e238393afeefeaae61c5
jotkafomat <k-j@xl.wp.pl>
2021-02-24 15:13:15 +0000

    Initial Commit

---
+++ MovieAPI/MovieAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAPIApp.swift
+//  MovieAPI
+//
+//  Created by Krzysztof Jankowski on 24/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct MovieAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
